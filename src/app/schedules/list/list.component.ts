213be8b364d99de4a45fcfd7fbd3116078df3a8a
Jacson S. Correa <jacsonlinux@gmail.com>
2019-06-18 13:34:04 -0300

    ...

--- src/app/schedules/list/list.component.ts
+++ src/app/schedules/list/list.component.ts
@@ -46,7 +46,7 @@ export class ListComponent implements OnInit {
   }
 
 
-  enviar() {
+  /*enviar() {
     const data = {
       place: 'j5XeONPpvQIBEzd6JXGU',
       user: 'gZzVb7Cs9HcXoX8NvtUoalOZB2R2',
@@ -54,7 +54,7 @@ export class ListComponent implements OnInit {
       endTime: '2019-05-14T20:54:00.000Z'
     };
     this.schedulesService.newSchedule(data).then(res => { console.log(res); }).catch(err => err.message);
-  }
+  }*/
 
   ngOnInit() {
     this.schedule.place = this.activatedRoute.snapshot.paramMap.get('id');
