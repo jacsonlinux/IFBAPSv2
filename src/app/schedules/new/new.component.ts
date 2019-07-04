0ae0c64f1d3fc9388ac5c0f8f23d763370f16b43
jacsonlinux <jacsonlinux@gmail.com>
2019-07-04 20:37:40 -0300

    ...

--- src/app/schedules/new/new.component.ts
+++ src/app/schedules/new/new.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MzToastService } from 'ngx-materialize';
@@ -6,15 +6,16 @@ import { SchedulesService } from '../schedules.service';
 import { Schedule } from '../../class/Schedule';
 import { AuthenticationService } from '../../authentication/authentication.service';
 import { Location } from '@angular/common';
+import { Subscription } from 'rxjs-compat/Subscription';
 
 @Component({
   selector: 'app-new',
   templateUrl: './new.component.html',
   styleUrls: ['./new.component.scss']
 })
-export class NewComponent implements OnInit {
+export class NewComponent implements OnInit, OnDestroy {
 
-  schedule: Schedule = new Schedule();
+  schedule = new Schedule();
 
   errorMessageResources = {
     dateSchedule: {
@@ -34,6 +35,8 @@ export class NewComponent implements OnInit {
   dateSchedule;
   start;
   end;
+
+  subscription: Subscription;
 
   public timepickerOptions: Pickadate.TimeOptions = {
     default: 'now',
@@ -86,37 +89,35 @@ export class NewComponent implements OnInit {
     });
   }
 
-  /* onSubmit() {
+  validatePeriod = (testPeriod, periods): boolean => {
+    for (let i = 0; i < periods.length; i++) {
+      const period = periods[i];
+      if (period.start < testPeriod.start && period.end > testPeriod.start) {
+        return false;
+      }
+      if (period.start > testPeriod.start && period.start < testPeriod.end) {
+        return false;
+      }
+    }
+    return true;
+  }
 
-     const data = this.scheduleForm.value;
-
-     this.dateSchedule = data.dateSchedule;
-     this.start = new Date(this.dateSchedule + ' ' + data.start + ':00');
-     this.end = new  Date(this.dateSchedule + ' ' + data.end + ':00');
-
-     this.schedule.start = this.start;
-     this.schedule.end = this.end;
-
-     if (this.start >= this.end) {
-       this.toastService.show('Start can not be greater than or equal to end', 5000, 'red fontArial white-text');
-       this.scheduleForm.reset();
-     } else {
-       this.showForm = false;
-       this.scheduleService
-         .newSchedule(JSON.stringify(this.schedule))
-         .then(res => {
-           if (res === true) {
-             this.toastService.show('Registered schedule!', 3000, 'green fontArial white-text');
-             this.location.back();
-           } else {
-             this.toastService.show(`${res}`, 3000, 'red fontArial white-text');
-             this.showForm = true;
-             this.buildForm();
-           }
-         })
-         .catch(err => err.message);
-     }
-   }*/
+  newSchedule(schedule): void {
+    this.subscription.unsubscribe();
+    this.scheduleService
+      .newSchedule(schedule)
+      .then(res => {
+        if (res === true) {
+          this.toastService.show('Registered schedule!', 3000, 'green fontArial white-text');
+          this.location.back();
+        } else {
+          this.toastService.show(`${res}`, 3000, 'red fontArial white-text');
+          this.showForm = true;
+          this.buildForm();
+        }
+      })
+      .catch(err => err.message);
+  }
 
   onSubmit() {
     const data = this.scheduleForm.value;
@@ -131,31 +132,44 @@ export class NewComponent implements OnInit {
       this.scheduleForm.reset();
     } else {
       this.showForm = false;
-      this.scheduleService
-        .newSchedule({
-          end: this.schedule.end,
-          place: this.schedule.place,
-          start: this.schedule.start,
-          user: this.schedule.user,
-          title: this.schedule.title
-        })
-        .then(res => {
-          if (res === true) {
-            this.toastService.show('Registered schedule!', 3000, 'green fontArial white-text');
-            this.location.back();
-          } else {
-            this.toastService.show(`${res}`, 3000, 'red fontArial white-text');
-            this.showForm = true;
-            this.buildForm();
+      const schedule = {
+        end: this.schedule.end,
+        place: this.schedule.place,
+        start: this.schedule.start,
+        user: this.schedule.user,
+        title: this.schedule.title
+      };
+      this.subscription = this.scheduleService.validateSchedule(schedule).subscribe(schedules => {
+        if (schedules.length === 0) {
+          this.newSchedule(schedule);
+        } else {
+          const periods = [];
+          for (const entry of schedules) {
+            periods.push({
+              start: entry.data.start.toDate(),
+              end: entry.data.end.toDate()
+            });
           }
-        })
-        .catch(err => err.message);
+          const period = {
+            start: schedule.start,
+            end: schedule.end
+          };
+          const valid = this.validatePeriod(period, periods);
+          if (valid) {
+            this.newSchedule(schedule);
+          } else {
+            this.subscription.unsubscribe();
+            this.toastService.show('Invalid period schedule!', 3000, 'red fontArial white-text');
+            this.showForm = true;
+          }
+        }
+      });
     }
   }
 
-  // newSchedule() { this.scheduleService.validateSchedule(this.schedule); }
-  // this.scheduleService.validateSchedule().subscribe(res => res);
-
+  ngOnDestroy() {
+    // this.subscription.unsubscribe();
+  }
 
   ngOnInit() {
     this.buildForm();
@@ -165,23 +179,3 @@ export class NewComponent implements OnInit {
   }
 
 }
-
-/*this.schedules.subscribe(schedules => {
-      if (schedules.length === 0) {
-        console.log('pode agendar esse periodo!');
-      } else {
-        const periods = [];
-        for (const entry of schedules) {
-          periods.push({
-            start: entry.data.start.toDate(),
-            end: entry.data.end.toDate()
-          });
-        }
-        const testPeriod = {
-          start: schedule.start,
-          end: schedule.end
-        };
-        const x = this.validatePeriod(testPeriod, periods);
-        console.log(x);
-      }
-    });*/
