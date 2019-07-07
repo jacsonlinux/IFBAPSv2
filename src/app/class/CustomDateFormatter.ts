dc1d9e136e2253ea6834c766cf7cc7edc992c37a
jacsonlinux <jacsonlinux@gmail.com>
2019-07-07 16:48:10 -0300

    ...

--- src/app/class/CustomDateFormatter.ts
+++ src/app/class/CustomDateFormatter.ts
@@ -1,4 +1,5 @@
-import { CalendarDateFormatter, DateFormatterParams } from 'angular-calendar';
+
+/*import { CalendarDateFormatter, DateFormatterParams } from 'angular-calendar';
 import { DatePipe } from '@angular/common';
 
 export class CustomDateFormatter extends CalendarDateFormatter {
@@ -10,5 +11,30 @@ export class CustomDateFormatter extends CalendarDateFormatter {
 
   public weekViewHour({ date, locale }: DateFormatterParams): string {
     return this.dayViewHour({ date, locale });
+  }
+}*/
+
+
+
+import { CalendarDateFormatter, DateFormatterParams } from 'angular-calendar';
+import { DatePipe } from '@angular/common';
+
+export class CustomDateFormatter extends CalendarDateFormatter {
+  // you can override any of the methods defined in the parent class
+
+  public monthViewColumnHeader({ date, locale }: DateFormatterParams): string {
+    return new DatePipe(locale).transform(date, 'EEE', locale);
+  }
+
+  /*public monthViewTitle({ date, locale }: DateFormatterParams): string {
+    return new DatePipe(locale).transform(date, 'MMM y', locale);
+  }*/
+
+  public weekViewColumnHeader({ date, locale }: DateFormatterParams): string {
+    return new DatePipe(locale).transform(date, 'EEE', locale);
+  }
+
+  public dayViewHour({ date, locale }: DateFormatterParams): string {
+    return new DatePipe(locale).transform(date, 'HH:mm', locale);
   }
 }
