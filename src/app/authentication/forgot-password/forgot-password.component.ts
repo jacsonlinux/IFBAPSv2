c3b9898c5127b2e171b012f10c572169234d865c
Daniel Demarco <danieldemarco321@gmail.com>
2019-07-30 13:56:29 -0300

    forgot-password

--- src/app/authentication/forgot-password/forgot-password.component.ts
+++ src/app/authentication/forgot-password/forgot-password.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+import {AuthenticationService} from '../authentication.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.scss']
 })
 export class ForgotPasswordComponent implements OnInit {
+  passReset: boolean;
+  userForm: any;
+  showFormLogin: boolean;
+  logInForm: FormGroup;
+  errorMessageResources = {
+    email: {
+      required: 'Email is required.',
+      pattern: 'It should be from the domain @ ifba.edu.br.',
+      email: 'Must be a valid email.'
+    }
+  };
+  constructor(
+    private formBuilder: FormBuilder,
+    private authenticationService: AuthenticationService
+  ) {}
 
-  constructor() { }
+  resetPassword() {
+    this.authenticationService.resetPassword(this.userForm.value.email).then(() => this.passReset = true);
+  }
+
+  buildForm() {
+    this.logInForm = this.formBuilder.group({
+      email: [null, Validators.compose([
+        Validators.required,
+        Validators.pattern('[a-z0-9._%+-]+@ifba.edu.br'),
+        Validators.email
+      ])],
+      password: [null, Validators.compose([Validators.required])],
+    });
+  }
 
   ngOnInit() {
+    this.buildForm();
+    this.showFormLogin = true;
   }
 
 }
