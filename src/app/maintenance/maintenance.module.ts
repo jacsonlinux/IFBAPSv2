f033a875932963fb4b50e78ef896d721655a5a5b
daniel <danieldemarco321@gmail.com>
2019-09-16 16:38:12 -0300

    qr scanner

--- src/app/maintenance/maintenance.module.ts
+++ src/app/maintenance/maintenance.module.ts
@@ -4,12 +4,14 @@ import { MaintenanceComponent } from './maintenance.component';
 import { RepairComponent } from './repair/repair.component';
 import { MaintenanceService } from "./maintenance.service";
 import { MaintenanceRoutingModule } from "./maintenance-routing.module";
+import {NgQrScannerModule} from 'angular2-qrscanner';
 
 @NgModule({
   declarations: [MaintenanceComponent, RepairComponent],
   imports: [
     CommonModule,
-    MaintenanceRoutingModule
+    MaintenanceRoutingModule,
+    NgQrScannerModule
   ],
   providers: [MaintenanceService]
 })
