5faabfd9e1c7acf9b07375bd38b617ee807d6f90
Jacson S. Correa <jacsonlinux@gmail.com>
2019-09-18 16:48:14 -0300

    ...

--- src/app/maintenance/maintenance.module.ts
+++ src/app/maintenance/maintenance.module.ts
@@ -4,7 +4,7 @@ import { MaintenanceComponent } from './maintenance.component';
 import { RepairComponent } from './repair/repair.component';
 import { MaintenanceService } from "./maintenance.service";
 import { MaintenanceRoutingModule } from "./maintenance-routing.module";
-import {NgQrScannerModule} from 'angular2-qrscanner';
+import { NgQrScannerModule } from 'angular2-qrscanner';
 
 @NgModule({
   declarations: [MaintenanceComponent, RepairComponent],
