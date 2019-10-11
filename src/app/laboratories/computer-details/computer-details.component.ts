d0a8e21cf1200a9944279ee6ba2303c0eb5ff4a3
Jacson S. Correa <jacsonlinux@gmail.com>
2019-10-11 14:30:13 -0300

    ...

--- src/app/laboratories/computer-details/computer-details.component.ts
+++ src/app/laboratories/computer-details/computer-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { LaboratoriesService } from '../laboratories.service';
 import {AppService} from '../../app.service';
+import {MzToastService} from 'ngx-materialize';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-computer-details',
@@ -11,20 +13,35 @@ import {AppService} from '../../app.service';
 export class ComputerDetailsComponent implements OnInit {
 
   computer;
+  laboratory;
 
   constructor(
     private laboratoriesService: LaboratoriesService,
     private appService: AppService,
     private location: Location,
+    private router: Router,
+    private toastService: MzToastService
   ) { }
 
   ngOnInit() {
+    this.laboratoriesService.currentLaboratory.subscribe(res => this.laboratory = res.id);
     this.appService.changePlaceTitle('COMPUTER DETAILS');
     this.laboratoriesService.currentComputer.subscribe(computer => {
       if (computer === null) { this.location.back(); } else {
         this.computer = computer;
       }
     });
+  }
+
+  repair(uuid) {
+    this.laboratoriesService.repair(this.laboratory, uuid).then(res => {
+      this.location.back();
+      if (res.status === 1) {
+        this.toastService.show(`${res.message}`, 5000, 'green white-text center');
+      } else if (res.status === 2 ) {
+        this.toastService.show(`${res.message}`, 5000, 'red darken-3 white-text center');
+      }
+    }).catch(err => err.message);
   }
 
 }
